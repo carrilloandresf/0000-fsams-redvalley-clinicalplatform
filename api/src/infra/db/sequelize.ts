ed638def033c5aad9a3e3458acd1adf37e70f16f
Felipe Carrillo <anfelcaro@hotmail.com>
2025-10-22 19:00:32 -0500

    react implemented, fix nx-package, start frontend

--- api/src/infra/db/sequelize.ts
+++ api/src/infra/db/sequelize.ts
@@ -5,5 +5,15 @@ export const sequelize = new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASS, {
   host: env.DB_HOST,
   port: env.DB_PORT,
   dialect: 'mysql',
+  dialectModule: require('mysql2'),
   logging: false,
+  dialectOptions: {
+    connectTimeout: 60000
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 });
